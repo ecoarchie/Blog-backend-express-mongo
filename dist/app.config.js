aad70b0c05196de91c65d06bac62baefd64c40f1
ecoarchie <eco.artur@gmail.com>
2022-12-24 23:28:34 +0300

    feat: user auth flow

--- dist/app.config.js
+++ dist/app.config.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -29,6 +38,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.port = exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const blog_routers_1 = require("./routes/blog-routers");
 const post_routers_1 = require("./routes/post-routers");
 const user_routes_1 = require("./routes/user-routes");
@@ -43,12 +53,26 @@ exports.app.use(body_parser_1.default.json());
 exports.app.use('/blogs', blog_routers_1.blogRouter);
 exports.app.use('/posts', post_routers_1.postRouter);
 exports.app.use('/users', user_routes_1.userRouter);
-exports.app.get('/', (req, res) => {
+exports.app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.send('Hello World!');
-});
-exports.app.delete('/testing/all-data', (req, res) => {
+}));
+exports.app.post('/auth/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userPassword = req.body.password;
+    const userLoginOrEmail = req.body.loginOrEmail;
+    const userPasswordInDB = 'qwerty1';
+    const userLoginInDB = 'lg-465115';
+    const hash = yield bcrypt_1.default.hash(userPassword, 1);
+    const match = yield bcrypt_1.default.compare(userPasswordInDB, hash);
+    if (match && userLoginOrEmail === userLoginInDB) {
+        res.sendStatus(204);
+    }
+    else {
+        res.sendStatus(401);
+    }
+}));
+exports.app.delete('/testing/all-data', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     blogs_repository_1.blogsRepository.deleteAllBlogs();
     posts_repository_1.postsRepository.deleteAllPosts();
     users_repository_1.usersRepository.deleteAllUsers();
     res.sendStatus(204);
-});
+}));
--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,6 @@
 import express, { Response, Request } from 'express';
 import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
 import { blogRouter } from './routes/blog-routers';
 import { postRouter } from './routes/post-routers';
 import { userRouter } from './routes/user-routes';
@@ -7,6 +8,7 @@ import { blogsRepository } from './repositories/blogs-repository';
 import { postsRepository } from './repositories/posts-repository';
 import { usersRepository } from './repositories/users-repository';
 import * as dotenv from 'dotenv';
+import { usersCollection } from './repositories/db';
 
 dotenv.config();
 
@@ -18,11 +20,28 @@ app.use('/blogs', blogRouter);
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
-app.delete('/testing/all-data', (req: Request, res: Response) => {
+app.post('/auth/login', async (req: Request, res: Response) => {
+  const userPassword = req.body.password;
+  const userLoginOrEmail = req.body.loginOrEmail;
+
+  const userPasswordInDB = 'qwerty1';
+  const userLoginInDB = 'lg-465115';
+
+  const hash = await bcrypt.hash(userPassword, 1);
+
+  const match = await bcrypt.compare(userPasswordInDB, hash);
+  if (match && userLoginOrEmail === userLoginInDB) {
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.delete('/testing/all-data', async (req: Request, res: Response) => {
   blogsRepository.deleteAllBlogs();
   postsRepository.deleteAllPosts();
   usersRepository.deleteAllUsers();
